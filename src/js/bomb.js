3d016e12d195e4e993164d548391420b0bf508cf
JJ Yang <rongyang610@gmail.com>
2019-04-15 11:17:00 -0400

    Added all sides explode now to add 3 bombs per player and also sasuke

--- src/js/bomb.js
+++ src/js/bomb.js
@@ -39,18 +39,60 @@ class bomb{
 
   explodeBomb(){
     this.drawBomb(5, 3, this.xPos, this.yPos);
-    this.explodeUpWard();
+    this.explodeUp();
+    this.explodeDown();
+    this.explodeLeft();
+    this.explodeRight();
   }
 
-  explodeUpWard(){
+  explodeUp(){
     for (let j = this.yPos - 1; j > 0; j--) {
-      if (j === 1){
+      if(j % 2 === 0 && this.xPos % 2 === 0){
+        break;
+      } else if(j === 1){
         this.drawBomb(7, 1, this.xPos, j);
       } else{
         this.drawBomb(5, 4, this.xPos, j);
       }
     }
   }
+
+  explodeDown(){
+    for (let j = this.yPos + 1; j < 14; j++) {
+      if(j % 2 === 0 && this.xPos % 2 === 0){
+        break;
+      } else if(j === 13){
+        this.drawBomb(5, 5, this.xPos, j);
+      } else{
+        this.drawBomb(5, 4, this.xPos, j);
+      }
+    }
+  }
+
+  explodeLeft(){
+    for (let i = this.xPos - 1; i > 0; i--) {
+      if(i % 2 === 0 && this.yPos % 2 === 0){
+        break;
+      }else if (i === 1){
+        this.drawBomb(4, 1, i, this.yPos);
+      } else{
+        this.drawBomb(0, 3, i, this.yPos);
+      }
+    }
+  }
+
+  explodeRight(){
+    for (let i = this.xPos + 1; i < 14; i++) {
+      if(i % 2 === 0 && this.yPos % 2 === 0){
+        break;
+      } else if (i === 13){
+        this.drawBomb(1, 3, i, this.yPos);
+      } else{
+        this.drawBomb(0, 4, i, this.yPos);
+      }
+    }
+  }
+
 }
 
 export default bomb;
